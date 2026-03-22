f62f8640ec97a0753f2478cac4c067355f2f7943
Шестопалов Пётр <shestikpetr@gmail.com>
2026-03-22 17:39:33 +0700

    docs: добавлен README

--- user-service/src/main/kotlin/com/shestikpetr/userservice/repositories/GroupMembershipRepository.kt
+++ user-service/src/main/kotlin/com/shestikpetr/userservice/repositories/GroupMembershipRepository.kt
@@ -3,7 +3,9 @@ package com.shestikpetr.userservice.repositories
 import com.shestikpetr.userservice.entities.GroupMembership
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
+@Repository
 interface GroupMembershipRepository : JpaRepository<GroupMembership, Long> {
     fun findByGroupId(groupId: Long): List<GroupMembership>
     fun findByUserId(userId: Long): List<GroupMembership>
